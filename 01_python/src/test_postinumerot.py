ff061ab0f5cd60c16462828d1b25d55f1b9594d6
Teemu H <swd1tn002@users.noreply.github.com>
2021-02-11 10:07:43 +0200

    Testien siistiminen Pytestin fixture-dekoraattorilla

--- 01_python/src/test_postinumerot.py
+++ 01_python/src/test_postinumerot.py
@@ -1,52 +1,56 @@
+import pytest
 import postinumerot
 
 POSTINUMEROT = {
-    "74701": "KIURUVESI",
     "35540": "JUUPAJOKI",
     "74700": "KIURUVESI",
+    "74701": "KIURUVESI",
     "73460": "MUURUVESI"
 }
 
 ERIKOISTAPAUKSET = {
     "43800": "KIVIJÄRVI",
-    "91150": "YLI-OLHAVA",
     "65374": "SMART POST",
+    "74704": "SMARTPOST",
     "90210": "BEVERLY HILLS",
-    "74704": "SMARTPOST"
+    "91150": "YLI-OLHAVA",
+    "96204": "smart-post"
 }
 
 
-def test_ryhmittele_yksittainen_postinumero():
-    toimipaikat = postinumerot.ryhmittele_toimipaikoittain(POSTINUMEROT)
-
-    assert toimipaikat["JUUPAJOKI"] == ["35540"]
-
-
-def test_ryhmittele_useita_postinumeroita():
-    toimipaikat = postinumerot.ryhmittele_toimipaikoittain(POSTINUMEROT)
-
-    assert toimipaikat["KIURUVESI"] == ["74701", "74700"]
+@pytest.fixture
+def ryhmitelty():
+    kaikki = {**POSTINUMEROT, **ERIKOISTAPAUKSET}
+    return postinumerot.ryhmittele_toimipaikoittain(kaikki)
 
 
-def test_ryhmittely_tuottaa_oikean_maaran_ryhmia():
-    toimipaikat = postinumerot.ryhmittele_toimipaikoittain(POSTINUMEROT)
-    assert len(toimipaikat) == 3
+def test_ryhmittele_yksittainen_postinumero(ryhmitelty):
+    assert ryhmitelty["JUUPAJOKI"] == ["35540"]
 
 
-def test_ryhmittele_toimipaikkojen_erikoistapaukset():
-    toimipaikat = postinumerot.ryhmittele_toimipaikoittain(ERIKOISTAPAUKSET)
-
-    assert "43800" in toimipaikat["KIVIJÄRVI"]
-    assert "65374" in toimipaikat["SMARTPOST"]
-    assert "91150" in toimipaikat["YLIOLHAVA"]
+def test_ryhmittele_useita_postinumeroita(ryhmitelty):
+    assert ryhmitelty["KIURUVESI"] == ["74700", "74701"]
 
 
-def test_ryhmittely_ei_huomioi_valimerkkeja_eika_kirjainkokoa():
-    smart_postit = {
-        "65374": "SMART POST",
-        "74704": "SMARTPOST",
-        "96204": "smart-post"
-    }
-    toimipaikat = postinumerot.ryhmittele_toimipaikoittain(smart_postit)
+def test_ryhmittele_toimipaikkojen_erikoistapaukset(ryhmitelty):
+    assert "43800" in ryhmitelty["KIVIJÄRVI"]
+    assert "65374" in ryhmitelty["SMARTPOST"]
+    assert "91150" in ryhmitelty["YLIOLHAVA"]
 
-    assert toimipaikat["SMARTPOST"] == ["65374", "74704", "96204"]
+
+def test_ryhmittely_ei_huomioi_valimerkkeja_eika_kirjainkokoa(ryhmitelty):
+    assert ryhmitelty["SMARTPOST"] == ["65374", "74704", "96204"]
+
+
+def test_etsi_postinumero_toimii_kirjainkoosta_riippumatta(ryhmitelty):
+    numerot = postinumerot.etsi_postinumerot('Kivijärvi', ryhmitelty)
+
+    assert numerot == ["43800"]
+
+
+def test_etsi_smart_post_eri_kirjoitsasuilla(ryhmitelty):
+    SMARTPOST = postinumerot.etsi_postinumerot('SMARTPOST', ryhmitelty)
+    smartpost = postinumerot.etsi_postinumerot('smartpost', ryhmitelty)
+    smart_post = postinumerot.etsi_postinumerot('SMART POST', ryhmitelty)
+
+    assert SMARTPOST == smartpost == smart_post == ["65374", "74704", "96204"]
