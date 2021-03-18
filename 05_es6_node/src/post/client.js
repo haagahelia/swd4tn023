3a196022f798eae22fd436638865a5998db28765
Teemu H <swd1tn002@users.noreply.github.com>
2021-03-18 14:07:11 +0200

    Lisätty node-cache postinumero JSON:ille

--- 05_es6_node/src/post/client.js
+++ 05_es6_node/src/post/client.js
@@ -1,17 +1,31 @@
 const fetch = require('node-fetch');
+const NodeCache = require('node-cache');
+
+const cache = new NodeCache();
 
 async function loadJson() {
     let response = await fetch('https://raw.githubusercontent.com/theikkila/postinumerot/master/postcode_map_light.json');
     return await response.json();
 }
 
+async function loadCachedJson() {
+    let cached = cache.get('postalJson');
+    if (cached) {
+        return cached;
+    } else {
+        let data = await loadJson();
+        cache.set('postalJson', data);
+        return data;
+    }
+}
+
 async function getPostalDistrict(postalCode) {
-    let data = await loadJson();
+    let data = await loadCachedJson();
     return data[postalCode.toUpperCase()] || null;
 }
 
 async function getPostalCodes(district) {
-    let data = await loadJson();
+    let data = await loadCachedJson();
     return Object.entries(data)
         .filter(([code, name]) => name.toUpperCase() === district.toUpperCase())
         .map(([code, name]) => code);
